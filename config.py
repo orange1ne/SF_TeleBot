c696a2af27d0ca208bdde60ce5ef94f4e8f78b12
Kirill <k.krikliviy@gmail.com>
2022-07-16 18:34:39 +0300

    Created Telegram Bot Project

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5567256214:AAGIgtoObfarsx8XLojcncEelNId-W1XTpU'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CurrConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIException(f'Нельзя конвертировать одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount.replace(',', '.'))
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        total_base = float(total_base) * amount
+
+        return round(total_base, 3)
